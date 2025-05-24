08fbb99cbc6f055789774ac92eb751bc6c5031e5
SBumhe <sheltonbumhe@gmail.com>
2025-05-24 09:53:51 -0500

    Update App.java

---
+++ backend-java/src/com/trendpredictor/App.java
@@ -0,0 +1,7 @@
+package com.trendpredictor;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("AI-Driven Trend Prediction System backend is running.");
+    }
+}
